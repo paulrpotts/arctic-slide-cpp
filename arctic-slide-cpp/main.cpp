1045c9e3924a4b85cd9b9fe2385fda5008c09ff0
Paul R. Potts <paul@thepottshouse.org>
2013-08-01 14:32:38 -0400

    Initial Commit

---
+++ arctic-slide-cpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  arctic-slide-cpp
+//
+//  Created by Paul R. Potts on 8/1/13.
+//  Copyright (c) 2013 Paul R. Potts. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
